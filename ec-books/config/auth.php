f4461138f417275a9a86d67112223e77d486becb
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-09-15 23:37:00 +0900

    管理者用ログインの設定

--- ec-books/config/auth.php
+++ ec-books/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'user',
         'passwords' => 'users',
     ],
 
@@ -46,6 +46,16 @@ return [
             'provider' => 'users',
             'hash' => false,
         ],
+
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
+        'admin' => [ //追加
+            'driver' => 'session', //追加
+            'provider' => 'admins', //追加
+        ],
     ],
 
     /*
@@ -69,6 +79,11 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
+        ],
+
+        'admins' => [ //追加
+            'driver' => 'eloquent', //追加
+            'model' => App\Admin::class, //追加
         ],
 
         // 'users' => [
