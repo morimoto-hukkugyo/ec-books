aee7d1c8d4ba3042088d89060d716bb0eb3e5a20
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-09-26 23:51:32 +0900

    user追加

--- ec-books/database/migrations/2014_10_12_000000_create_users_table.php
+++ ec-books/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('last_name_kana');
+            $table->string('first_name_kana');
+            $table->integer('postal_code');
+            $table->string('addres');
+            $table->string('telephone_number');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
