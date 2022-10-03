22c3895cf34f638dc06be5d23e903d400fad9751
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-10-03 23:01:07 +0900

    商品テーブルの作成

---
+++ ec-books/database/migrations/2022_10_03_222937_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('name', 100);
+            $table->string('image', 4);
+            $table->string('introduction', 1000);
+            $table->integer('price');
+            $table->string('writer_name', 30);
+            $table->string('writer_intro', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
