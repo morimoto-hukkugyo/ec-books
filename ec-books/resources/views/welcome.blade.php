720ba400f837da4b7bc86fd8bc0f42748f5ded91
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-10-03 19:37:32 +0900

    <div class=topics>まで作成

--- ec-books/resources/views/welcome.blade.php
+++ ec-books/resources/views/welcome.blade.php
@@ -1,100 +1,47 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="col-10">
+                {{-- いつかスライドショー --}}
+                <div class="slider">
+                    <img src="{{ asset('img/夜行.jpg') }}" height="500" alt="">
+                    <img src="{{ asset('img/medium.jpg') }}" height="500" alt=""> 
+                    <img src="{{ asset('img/WE11DONE_L.jpg') }}" height="500" alt=""> 
+                    <img src="{{ asset('img/balenciaga.jpg') }}"  height="500" alt="">
+                    <img src="{{ asset('img/fear-of-god.jpg') }}"  height="500" alt="">
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                {{-- オススメ --}}
+                <div class="topics">
+                    <div class="event_fream">
+                        <div class="event_box_l">
+                            <a href="https://www.amazon.co.jp/%E5%A4%9C%E8%A1%8C-%E6%A3%AE%E8%A6%8B-%E7%99%BB%E7%BE%8E%E5%BD%A6/dp/409386456X">
+                                <img src="{{ asset('img/夜行.jpg')}}" height="140" width="215">
+                            </a>
+                            <a href="https://www.amazon.co.jp/%E5%A4%9C%E8%A1%8C-%E6%A3%AE%E8%A6%8B-%E7%99%BB%E7%BE%8E%E5%BD%A6/dp/409386456X" class="event_box_link">
+                                <div class="event_box">
+                                    <img src="{{ asset('img/icon_item.jpg') }}">
+                                    <div class="txt_sub">
+                                        <p>夜行</p>
+                                        <br>
+                                    </div>
+                                    <div class="text_main">
+                                        <p>作者:森見</p>
+                                        {{-- ￥{{ $item->price}} --}}
+                                        <p>値段: 1000</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="event_box_l">
+                            <img src="{{ asset('img/medium.jpg')}}" height="140" width="215">
+                            
+                        </div>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+              </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>    
+@endsection
