81ad75cfd5fe398dbc204fd917431fd0bfc6bc4c
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-10-07 00:19:23 +0900

    foreach作成

--- ec-books/resources/views/welcome.blade.php
+++ ec-books/resources/views/welcome.blade.php
@@ -14,155 +14,58 @@
                       </div>
                     </div>
                 </div>
-                {{-- オススメ --}}
+                {{-- 新着 --}}
                 <div class="topics">
                     <div class="event_fream">
+                        <h3>新着商品</h3>
+                        <img src="{{ asset('img/title_border.jpg') }}">
+                        @foreach ($products as $product)
                         <div class="event_box_l">
                             <a href="https://www.amazon.co.jp/%E5%A4%9C%E8%A1%8C-%E6%A3%AE%E8%A6%8B-%E7%99%BB%E7%BE%8E%E5%BD%A6/dp/409386456X">
-                                <img src="{{ asset('img/夜行.jpg')}}" height="140" width="215">
+                                {{-- 写真 --}}
+                                <img src="{{ Storage::url($product->image) }}" height="140px" width="215px">
                             </a>
                             <a href="https://www.amazon.co.jp/%E5%A4%9C%E8%A1%8C-%E6%A3%AE%E8%A6%8B-%E7%99%BB%E7%BE%8E%E5%BD%A6/dp/409386456X" class="event_box_link">
                                 <div class="event_box">
                                     <img src="{{ asset('img/icon_item.jpg') }}">
                                     <div class="txt_sub">
-                                        <p>夜行</p>
+                                        {{-- 商品名 --}}
+                                        {{ $product->name }}
                                         <br>
                                     </div>
                                     <div class="text_main">
-                                        <p>作者:森見</p>
-                                        {{-- ￥{{ $item->price}} --}}
-                                        <p>値段: 1000</p>
+                                        {{-- 作家名 --}}
+                                        <p>作者: {{ $product->writer_name}}</p>
+                                        {{-- 商品値段 --}}
+                                        <p>値段: {{ $product->price}}</p>
                                     </div>
                                 </div>
                             </a>
                         </div>
-                        <div class="event_box_l">
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X">
-                                <img src="{{ asset('img/medium.jpg')}}" height="140" width="215">
-                            </a>
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X" class="event_box_link">
-                                <div class="event_box">
-                                    <img src="{{ asset('img/icon_item.jpg') }}">
-                                    <div class="txt_sub">
-                                        <p>medium 霊媒探偵城塚翡翠</p>
-                                        <br>
-                                    </div>
-                                    <div class="text_main">
-                                        <p>作者:相沢 沙呼 </p>
-                                        {{-- ￥{{ $item->price}} --}}
-                                        <p>値段: 1870</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="event_box_l">
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X">
-                                <img src="{{ asset('img/medium.jpg')}}" height="140" width="215">
-                            </a>
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X" class="event_box_link">
-                                <div class="event_box">
-                                    <img src="{{ asset('img/icon_item.jpg') }}">
-                                    <div class="txt_sub">
-                                        <p>medium 霊媒探偵城塚翡翠</p>
-                                        <br>
-                                    </div>
-                                    <div class="text_main">
-                                        <p>作者:相沢 沙呼 </p>
-                                        {{-- ￥{{ $item->price}} --}}
-                                        <p>値段: 1870</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                    {{-- 2段目 --}}
-                    <div class="event_fream">
-                        <div class="event_box_l">
-                            <a href="https://www.amazon.co.jp/%E5%A4%9C%E8%A1%8C-%E6%A3%AE%E8%A6%8B-%E7%99%BB%E7%BE%8E%E5%BD%A6/dp/409386456X">
-                                <img src="{{ asset('img/夜行.jpg')}}" height="140" width="215">
-                            </a>
-                            <a href="https://www.amazon.co.jp/%E5%A4%9C%E8%A1%8C-%E6%A3%AE%E8%A6%8B-%E7%99%BB%E7%BE%8E%E5%BD%A6/dp/409386456X" class="event_box_link">
-                                <div class="event_box">
-                                    <img src="{{ asset('img/icon_item.jpg') }}">
-                                    <div class="txt_sub">
-                                        <p>夜行</p>
-                                        <br>
-                                    </div>
-                                    <div class="text_main">
-                                        <p>作者:森見</p>
-                                        {{-- ￥{{ $item->price}} --}}
-                                        <p>値段: 1000</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="event_box_l">
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X">
-                                <img src="{{ asset('img/medium.jpg')}}" height="140" width="215">
-                            </a>
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X" class="event_box_link">
-                                <div class="event_box">
-                                    <img src="{{ asset('img/icon_item.jpg') }}">
-                                    <div class="txt_sub">
-                                        <p>medium 霊媒探偵城塚翡翠</p>
-                                        <br>
-                                    </div>
-                                    <div class="text_main">
-                                        <p>作者:相沢 沙呼 </p>
-                                        {{-- ￥{{ $item->price}} --}}
-                                        <p>値段: 1870</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="event_box_l">
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X">
-                                <img src="{{ asset('img/medium.jpg')}}" height="140" width="215">
-                            </a>
-                            <a href="https://www.amazon.co.jp/medium-%E9%9C%8A%E5%AA%92%E6%8E%A2%E5%81%B5%E5%9F%8E%E5%A1%9A%E7%BF%A1%E7%BF%A0-%E7%9B%B8%E6%B2%A2-%E6%B2%99%E5%91%BC/dp/406517094X" class="event_box_link">
-                                <div class="event_box">
-                                    <img src="{{ asset('img/icon_item.jpg') }}">
-                                    <div class="txt_sub">
-                                        <p>medium 霊媒探偵城塚翡翠</p>
-                                        <br>
-                                    </div>
-                                    <div class="text_main">
-                                        <p>作者:相沢 沙呼 </p>
-                                        {{-- ￥{{ $item->price}} --}}
-                                        <p>値段: 1870</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="clear"></div>
+                        @endforeach
                     </div>
                 </div>
                 {{-- event end --}}
+
+                {{-- オススメの本 --}}
                 <div class="book_list">
                     <h3>オススメの本</h3>
-                    <img src="{{ asset('img/title_border.jpg') }}">
+                    {{-- <img src="{{ asset('img/title_border.jpg') }}"> --}}
                     <ul>
-                        <li class="first_child">
-                            <a href="https://www.amazon.co.jp/%E3%81%8B%E3%81%8C%E3%81%BF%E3%81%AE%E5%AD%A4%E5%9F%8E-%E8%BE%BB%E6%9D%91-%E6%B7%B1%E6%9C%88/dp/4591153320" target="_blank">
-                                <img src="{{ asset('img/かがみの弧城.jpg') }}">
-                                <span class="books_title">かがみの弧城</span>
-                                <span class="books_writer">辻村　深月</span>
-                                <span class="books_price">税込価格 1980円</span>
-                                <span class="books_ather"></span>
-                            </a>
-                        </li>
-                        <li class="first_child">
-                            <a href="https://www.amazon.co.jp/%E3%81%8B%E3%81%8C%E3%81%BF%E3%81%AE%E5%AD%A4%E5%9F%8E-%E8%BE%BB%E6%9D%91-%E6%B7%B1%E6%9C%88/dp/4591153320" target="_blank">
-                                <img src="{{ asset('img/夜行.jpg') }}">
-                                <span class="books_title">かがみの弧城</span>
-                                <span class="books_writer">辻村　深月</span>
-                                <span class="books_price">税込価格 1980円</span>
-                                <span class="books_ather"></span>
-                            </a>
-                        </li>
+                        @foreach ($osususme as $os)
+                            <li class="first_child">
+                                <a href="https://www.amazon.co.jp/%E3%81%8B%E3%81%8C%E3%81%BF%E3%81%AE%E5%AD%A4%E5%9F%8E-%E8%BE%BB%E6%9D%91-%E6%B7%B1%E6%9C%88/dp/4591153320" target="_blank">
+                                    <img src="{{ Storage::url($os->image) }}">
+                                    <span class="books_title">{{ $os->name }}</span>
+                                    <span class="books_writer">{{ $os->writer_name }}</span>
+                                    <span class="books_price">税込価格: {{ $os->price }}円</span>
+                                    <span class="books_ather"></span>
+                                </a>
+                            </li>
+                        @endforeach
                     </ul>
                 </div>
+                {{-- オススメの本終了 --}}
               </div>
         </div>
     </div>
