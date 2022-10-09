c3550239238e3798c5f7211f9bd07391db3bb1e9
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-10-09 13:03:22 +0900

    管理者ホーム、商品作成画面レイアウトの修正

--- ec-books/resources/views/layouts/app_admin.blade.php
+++ ec-books/resources/views/layouts/app_admin.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Life with Books') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -20,56 +20,50 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+    <!-- header -->
+    <header id="l-header">
+        <div class="p-mainHeader">
+            <div class="p-mainHeader__inner">
+                
+                <h1 class="p-mainHeader__tit">
+                    <a href="{{ url('/') }}">Life with Books</a>
+                </h1>
+                <div class="c-menuTrigger js-toggle-sp-menu">
+                    <span class="c-menuTrigger__bar js-toggle-sp-bar"></span>
+                    <span class="c-menuTrigger__bar js-toggle-sp-bar"></span>
+                    <span class="c-menuTrigger__bar js-toggle-sp-bar"></span>
+                </div>
+                <nav id="l-nav-menu" class="p-mainHeader__menu js-toggle-sp-menu-target">
+                    <ul class="c-menu">
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.login') }}">{{ __('Login') }}</a>
+                            <li class="c-menu__item js-toggle-sp-menu-link">
+                                <a class="p-mainHeader__btn" href="{{ route('admin.login') }}">{{ __('Login') }}</a>
                             </li>
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('admin.logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+                            <li class="c-menu__item js-toggle-sp-menu-link">
+                                <a class="p-mainHeader__btn" href="{{ route('admin.home') }}">マイページ</a>
                             </li>
+                            <li class="c-menu__item js-toggle-sp-menu-link">
+                                <a class="p-mainHeader__btn" href="{{ route('admin.logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                            </li>
+                            <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         @endguest
                     </ul>
-                </div>
+                </nav>
             </div>
-        </nav>
+        </div>
+    </header>{{-- /header --}}
+    
+    <main id="l-adminHome">
+        @yield('content')
+    </main>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
 </body>
 </html>
--- ec-books/resources/views/products/new.blade.php
+++ ec-books/resources/views/products/new.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app_admin')
 
 @section('content')
-<form action="{{ route('product.new') }}" class="c-form" method="post" enctype="multipart/form-data">
+<form action="{{ route('product.new') }}" id="l-adminForm" class="" method="post" enctype="multipart/form-data">
     @csrf
 
     <label for="name" class="c-form__label">
         タイトル
-        <input id="" name="name" type="text" maxlength="100" class="c-input js-count-title-input @error('title') error @enderror" value="" 
+        <input id="" name="name" type="text" maxlength="100" class="c-form__input js-count-title-input @error('title') error @enderror" value="" 
         required autocomplete="title" autofocus placeholder="本のタイトル" >
         <div class="c-form__msg">
             <p class="c-form__error">
@@ -18,13 +18,13 @@
     </label>
     <label for="category_id" class="c-form__label">
         カテゴリー
-        <select class="c-select @error('category_id') error @enderror" name="category_id" id="category_id">
+        <select class="c-form__input @error('category_id') error @enderror" name="category_id" id="category_id">
             <option value="0">未分類</option>
         </select>
         <div class="c-form__msg">
-            {{-- @error('category_id')
+            @error('category_id')
                 <p class="c-form__error">{{ $message }}</p>
-            @enderror --}}
+            @enderror
         </div>
     </label>
     <label for="category_id" class="c-form__label">
@@ -33,21 +33,21 @@
     </label>
     <label for="introduction" class="c-form__label">
         紹介
-        <textarea name="introduction" id="" cols="30" rows="10"></textarea>
+        <textarea name="introduction" id="" cols="30" rows="10" class="c-form__textarea"></textarea>
     </label>
     <label for="category_id" class="c-form__label">
         価格
-        <input name="price" type="text">
+        <input name="price" type="text" class="c-form__input">
     </label>
     <label for="writer_name" class="c-form__label">
         著者
-        <input name="writer_name" type="text">
+        <input name="writer_name" type="text" class="c-form__input">
     </label>
     <label for="writer_name" class="c-form__label">
         著者紹介
-        <textarea name="writer_intro" id="" cols="30" rows="10"></textarea>
+        <textarea name="writer_intro" id="" cols="30" rows="10" class="c-form__textarea"></textarea>
     </label>
     
-    <button class="c-form__submit c-btn" type="submit">作成</button>
+    <button class="c-form__submit" type="submit">作成</button>
 </form>
 @endsection
