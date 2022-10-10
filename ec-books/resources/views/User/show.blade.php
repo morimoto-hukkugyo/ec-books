68d417658b17b89e70477885b0874b0f7e46be9c
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-10-10 23:17:46 +0900

    詳細追加値段等

--- ec-books/resources/views/User/show.blade.php
+++ ec-books/resources/views/User/show.blade.php
@@ -2,15 +2,31 @@
 @section('content')
 <div class="conteiner">
     <div class="row">
-        <div class="col-5">
+        <div class="col-3">
             {{-- 詳細写真表示 --}}
-            <img src="{{ Storage::url($product->image) }}" height="435px">           
+            <div class="book_image">
+                <img src="{{ Storage::url($product->image) }}" height="435px"> 
+            </div>        
         </div>
         <div class="col-7">
             {{-- 商品詳細 --}}
             <div class="book-title-block">
                 <div class="book-title">
                     {{ $product->name}}
+                </div>
+                <div class="writer_name">
+                    著者: {{ $product->writer_name }}
+                </div>
+                <div class="writer_price">
+                    値段: {{ $product->price}}円
+                </div>
+                <div class="book_introduction">
+                    <h4>[商品詳細]</h2>
+                    {{ $product->introduction }}
+                </div>
+                <div class="book_writer_introduction">
+                    <h4>[著者紹介]</h2>
+                    {{ $product->writer_intro }}
                 </div>
             </div>
         </div>
