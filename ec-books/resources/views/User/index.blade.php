ac4f45bb47a452741df0ffb9966facce437f0cd2
morimoto-hukkugyo <senbenfuye@gmail.com>
2022-10-13 23:13:04 +0900

    商品一覧画面

--- ec-books/resources/views/User/index.blade.php
+++ ec-books/resources/views/User/index.blade.php
@@ -2,20 +2,44 @@
 @section('content')
 <div class="conteiner">
     <div class="row">
-        <div class="col-12">
-            <div class="row mx-auto mt-3">
-                @foreach ($products as $product)
-                    <div class="col-4">
-                        <img src="{{ Storage::url($product->image) }}" height="400px" width="90%">
-                        <div class="book-title">
-                            {{ $product->name}}
+        {{-- <div class="sidebar">
+            <h1>サイドバー</h1>
+        </div> --}}
+        @foreach ($products as $product)
+            {{-- <div class="user-index"> --}}
+                <a href="{{ route('user.show',['id'=>$product->id]) }}">
+                <div class="user-index">
+                    <div class="col-2">
+                        {{-- 詳細写真表示 --}}
+                        <div class="book_image">
+                            <img src="{{ Storage::url($product->image) }}" height="200px"> 
+                        </div>        
+                    </div>
+                    <div class="book-index">
+                        <div class="col-7">
+                            <div class="book-title">
+                                {{ $product->name}}
+                            </div>
+                            <div class="writername">
+                                著者: {{ $product->writer_name }}
+                            </div>
+                            <div class="writerprice">
+                                値段: {{ $product->price}}円
+                            </div>
+                            {{-- <div class="book_introduction">
+                                <h4>[商品詳細]</h2>
+                                {{ $product->introduction }}
+                            </div> --}}
+                            <div class="book_write_introduction">
+                                <h4>[著者紹介]</h2>
+                                {{ $product->writer_intro }}
+                            </div>
                         </div>
                     </div>
-                    
-                @endforeach
-            </div>
-            
-        </div>
+                </div>
+                </a>
+            {{-- </div> --}}
+        @endforeach
     </div>
 </div>
 @endsection
